146e17096fb151f6accdc731785d6c8effe1c154
guojianglin <1448373124@qq.com>
2020-05-18 00:53:40 +0800

    traval project init reset css/ mobile scale/ 1pixelborder/ 300ms touch delay

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <!-- <router-link to="/list">list</router-link> -->
+    </div>
+    <router-view/>
+  </div>
+</template>
